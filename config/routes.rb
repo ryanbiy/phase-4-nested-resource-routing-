2950f68aa74bb54476bb53fe8ab50cbd368cedb5
Ryan <ryanbiy01@gmail.com>
2023-07-08 11:24:21 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index, :show, :create]
+
+  resources :users, only: [:show, :index] do 
+    resources :items, only: [:index, :show, :create]
+  end
+
 end
